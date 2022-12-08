160d54a5c52fccb579bcce56a6610f1338d96f55
attaryu <mattarannaufal@gmail.com>
2022-12-08 11:55:43 +0700

    added test component: InputWithIcons.test.jsx

--- components/InputWithIcon.jsx
+++ components/InputWithIcon.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 
 function InputWithIcon({
   icon, type, name, placeholder, value, formHandler, minLength, maxLength,
 }) {
+  const refFocus = useRef();
+
+  const inputFocus = () => refFocus.current.focus();
+
   return (
     <label htmlFor={name} className="flex flex-row-reverse justify-center items-center mt-5 w-full h-10 rounded-md overflow-hidden">
       <input
@@ -15,11 +19,12 @@ function InputWithIcon({
         onChange={formHandler}
         minLength={minLength}
         maxLength={maxLength}
+        ref={refFocus}
         required
       />
 
-      <div className="h-full px-4 grid place-items-center bg-primary/10">
-        <img src={icon} alt="" className="w-7" />
+      <div className="h-full px-4 grid place-items-center bg-primary/10" onClick={inputFocus} aria-hidden>
+        <img src={icon} alt={`${name} icons`} className="w-7" onClick={inputFocus} aria-hidden />
       </div>
     </label>
   );
---
+++ components/InputWithIcon.test.jsx
@@ -0,0 +1,88 @@
+/*
+ Test Flow :
+  * InputWithIcon Component test
+    - input component value must be the same as typed
+    - the number of input component characters must be less and more than the specified
+      minimum/maximum limit
+    - when pressing icon should trigger to focus to component input
+*/
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import PropTypes from 'prop-types';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import InputWithIcon from './InputWithIcon';
+import useSimpleState from '../hooks/useSimpleState';
+
+const icon = '/asset/email.avif';
+const type = 'text';
+const name = 'inputForSomething';
+const placeHolder = 'Input for something...';
+
+function TestComponent({ maxCharacter, minCharacter }) {
+  const [state, handler] = useSimpleState('');
+
+  return (
+    <InputWithIcon
+      icon={icon}
+      type={type}
+      name={name}
+      placeholder={placeHolder}
+      value={state}
+      formHandler={handler}
+      maxLength={maxCharacter}
+      minLength={minCharacter}
+    />
+  );
+}
+
+TestComponent.propTypes = {
+  maxCharacter: PropTypes.string,
+  minCharacter: PropTypes.string,
+};
+
+TestComponent.defaultProps = {
+  maxCharacter: undefined,
+  minCharacter: undefined,
+};
+
+describe('InputWithIcon Component test', () => {
+  it('input component value must be the same as typed', async () => {
+    // arrage
+    render(<TestComponent />);
+    const inputElement = await screen.findByPlaceholderText(placeHolder);
+
+    // action
+    await userEvent.type(inputElement, 'i typed something');
+
+    // assert
+    expect(inputElement).toHaveValue('i typed something');
+  });
+
+  it('the number of input component characters must be less and more than the specified minimum/maximum limit', async () => {
+    // arrage
+    render(<TestComponent maxCharacter="20" minCharacter="10" />);
+    const inputElement = await screen.findByPlaceholderText(placeHolder);
+
+    // action
+    await userEvent.type(inputElement, 'i typed something');
+
+    // assert
+    expect(inputElement.value.length).toBeLessThanOrEqual(20);
+    expect(inputElement.value.length).toBeGreaterThanOrEqual(10);
+  });
+
+  it('when pressing icon should trigger to focus to component input', async () => {
+    // arrage
+    render(<TestComponent />);
+    const inputElement = await screen.findByPlaceholderText(placeHolder);
+    const imgElement = await screen.findByAltText(`${name} icons`);
+
+    // action
+    await userEvent.click(imgElement);
+
+    // assert
+    expect(inputElement).toHaveFocus();
+  });
+});
